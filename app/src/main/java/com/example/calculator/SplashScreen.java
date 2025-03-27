c1ef059b7b8ce5c1410dc74abd43805a438720e0
dew-13 <dewavishmi13@outlook.com>
2025-03-27 19:15:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/calculator/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.calculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+    public static int SPLASH_TIMER = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, CalculatorActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMER);
+    }
+
+}
